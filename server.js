11749b64b74c6ed5105314183308e052fee5981b
SebaAlex12 <sebastiank24@wp.pl>
2019-03-23 12:14:41 +0100

    d

--- server.js
+++ server.js
@@ -48,7 +48,8 @@ if (process.env.NODE_ENV === "production") {
   // set static folder
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.header("X-FRAME-OPTIONS", "ALLOW-FROM https://www.youtube.com");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("X-FRAME-OPTIONS", "ALLOW-FROM *");
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
